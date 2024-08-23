fb5c80564ab55d5c1b6c6ff335d214b46960991d
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-08-23 23:09:26 +0530

    feat -- added cards in the deck

--- app/(tabs)/deck.tsx
+++ app/(tabs)/deck.tsx
@@ -3,7 +3,17 @@ import { Colors } from "@/constants/Colors";
 import { api } from "@/convex/_generated/api";
 import { useMutation, useQuery } from "convex/react";
 import { Link, useRouter } from "expo-router";
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { FontAwesome, Ionicons, AntDesign } from "@expo/vector-icons";
+import { useEffect, useRef } from "react";
+import { Animated } from "react-native";
 
 export default function TabTwoScreen() {
   const window = Dimensions.get("window");
@@ -21,14 +31,14 @@ export default function TabTwoScreen() {
       style={{ backgroundColor: Colors.dark.background, flex: 1 }}
     >
       <Text className="text-2xl text-white font-bold">Deck</Text>
-      {cards?.map(({ name, _id }) => (
-        <Link
-          className="text-white text-2xl"
-          href={{ pathname: "/form", params: { flashCardID: _id } }}
-        >
-          {name}
-        </Link>
-      ))}
+      <FlatList
+        className="px-2 py-5"
+        ItemSeparatorComponent={() => <View className="my-3"></View>}
+        data={cards}
+        renderItem={({ item: { name, generating } }) => (
+          <Card title={name} generating={generating || false} cards={12} />
+        )}
+      />
       <View
         className="absolute"
         style={{ top: window.height - 120, left: window.width - 80 }}
@@ -36,6 +46,68 @@ export default function TabTwoScreen() {
         <IconButton onPress={create} style={styles.fab} icon="plus" />
       </View>
     </View>
+  );
+}
+
+interface CardProps {
+  title: string;
+  cards: number;
+  generating: boolean;
+  color?: string;
+}
+function Card({ title, cards, generating }: CardProps) {
+  return (
+    <TouchableOpacity
+      className="w-[95%] h-36 rounded-lg  px-6 py-3 justify-between relative"
+      style={{ backgroundColor: "#1B998B" }}
+    >
+      <View>
+        <Text className="text-base font-bold text-white">{title}</Text>
+        <Text className="text-base font-normal text-white">{cards} cards</Text>
+      </View>
+      <View className="flex flex-row items-center gap-2">
+        {generating ? (
+          <Loader />
+        ) : (
+          <>
+            <Text className="text-base font-medium text-white">Ready</Text>
+            <FontAwesome name="rocket" color="white" size={22} />
+          </>
+        )}
+      </View>
+      <View className="absolute right-0 top-[50%] mx-3">
+        <Ionicons name="chevron-forward-outline" color="white" size={30} />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+function Loader() {
+  const spinValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const spinAnimation = Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      })
+    );
+    spinAnimation.start();
+  }, [spinValue]);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
+  return (
+    <>
+      <Text className="text-base font-medium text-white mr-2">Ready</Text>
+      <Animated.View style={{ transform: [{ rotate: spin }] }}>
+        <AntDesign name="loading1" color="white" size={20} />
+      </Animated.View>
+    </>
   );
 }
 
